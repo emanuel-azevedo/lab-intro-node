0a2ddfa6985af659bb9e0df25af8dd573fa5ec2b
Emanuel Azevedo <emanuelcansancao@hotmail.com>
2022-07-08 13:08:34 -0300

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    if(item !== undefined){
+      this.items.push(item)
+      this.length++
+      this.items.sort((a,b) => a - b)      
+    }
+    return this.items
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos] === undefined) {
+      throw new Error('OutOfBounds');
+    }
+     return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(!this.items.length) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if(!this.items.length) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    if(!this.items.length) {
+      return 0;
+    }
+    return this.items.reduce((acc, curr) => acc + curr)
+  }
 
-  avg() {}
+  avg() {
+    if(!this.items.length) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum()/this.items.length
+  }
 }
 
 module.exports = SortedList;
+
+ 
